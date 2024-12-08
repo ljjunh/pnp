f312653c95cb453b9297ef793e0454ad5b30458e
sunsuking <sunsuking@gmail.com>
2024-12-08 16:24:23 +0900

    feat: 숙소 정보 Entity 설계 완료 및 데이터 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,32 @@
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import KakaoProvider from 'next-auth/providers/kakao';
-import GoogleProvider from 'next-auth/providers/google';
-import {prisma} from "@/lib/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import KakaoProvider from "next-auth/providers/kakao";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/lib/client";
 
 const handler = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    session: {
-        strategy: "jwt"
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID as string,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return { ...token, ...user };
     },
-    providers : [
-        KakaoProvider({
-            clientId: process.env.KAKAO_CLIENT_ID as string,
-            clientSecret: process.env.KAKAO_CLIENT_SECRET as string
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        })
-    ],
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                token.id = user.id;
-              }
-          return { ...token, ...user };
-        }
-    }
-})
+  },
+});
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
