6e4fa390e83828768401b5072fcfdd1c358b6c3e
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-12-12 18:26:23 +0900

    chore: 불필요한 함수 삭제

--- src/utils/cookies.ts
+++
@@ -1,9 +0,0 @@
-export const getAccessToken = () => {
-  if (typeof window === 'undefined') return null;
-
-  const cookies = document.cookie.split(';');
-  const tokenCookie = cookies.find((cookie) => cookie.trim().startsWith('accessToken='));
-  if (!tokenCookie) return null;
-
-  return tokenCookie.split('=')[1];
-};
