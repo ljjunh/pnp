fb3048cd8afa68deaa9a07d3b96e9973cb74b3cb
sunsuking <sunsuking@gmail.com>
2024-12-13 14:35:56 +0900

    s3: 연결 설정 추가

---
+++ src/lib/server/s3.ts
@@ -0,0 +1,68 @@
+import {
+  DeleteObjectCommand,
+  DeleteObjectsCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
+
+const BUCKET_NAME = process.env.BUCKET_NAME;
+const REGION = process.env.AWS_REGION;
+const client = new S3Client({ region: REGION });
+export const S3_BASE_URL = `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com`;
+
+/**
+ * S3에 파일을 업로드한다.
+ *
+ * @param {File[]} files 파일 목록
+ * @returns {Promise<string[]>} 업로드된 파일 키 목록
+ */
+export const upload = async (files: File[]): Promise<string[]> => {
+  return await Promise.all(
+    files.map(async (file: File) => {
+      const body = Buffer.from(await file.arrayBuffer());
+      const key = file.name;
+      const contentType = file.type || 'image/jpg';
+
+      await client.send(
+        new PutObjectCommand({
+          Bucket: BUCKET_NAME,
+          Key: key,
+          Body: body,
+          ContentType: contentType,
+        }),
+      );
+
+      return key;
+    }),
+  );
+};
+
+/**
+ * S3에 파일을 삭제한다.
+ *
+ * @param {string} key 파일 키
+ */
+export const remove = async (key: string) => {
+  await client.send(
+    new DeleteObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: key,
+    }),
+  );
+};
+
+/**
+ * S3에 파일을 여러 개 삭제한다.
+ *
+ * @param {string[]} keys 파일 키 목록
+ */
+export const removeAll = async (keys: string[]) => {
+  await client.send(
+    new DeleteObjectsCommand({
+      Bucket: BUCKET_NAME,
+      Delete: {
+        Objects: keys.map((key) => ({ Key: key })),
+      },
+    }),
+  );
+};
