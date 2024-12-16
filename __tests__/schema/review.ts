f92aa5d4af023329fe17c97d9bdc784e558cefed
sunsuking <sunsuking@gmail.com>
2024-12-16 15:27:14 +0900

    fix: coderabbit 기반의 코드 수정 추가

--- __tests__/schema/review.ts
+++ __tests__/schema/review.ts
@@ -1,89 +1,72 @@
-import { createReviewSchema } from '@/schemas';
+import { createReviewSchema, updateReviewSchema } from '@/schemas';
+import { z } from 'zod';
 
-describe('createReviewSchema', () => {
-  it('입력 값이 정상이라면 유효성 검사를 통과해야 합니다', () => {
-    const input = {
-      rating: 5,
-      content: '좋은 숙소였습니다',
-    };
+const testReviewSchema = (schemaName: string, schema: z.ZodSchema) => {
+  describe(schemaName, () => {
+    it('입력 값이 정상이라면 유효성 검사를 통과해야 합니다', () => {
+      const input = {
+        rating: 5,
+        content: '좋은 숙소였습니다',
+      };
 
-    expect(createReviewSchema.safeParse(input).success).toBeTruthy();
+      expect(schema.safeParse(input).success).toBeTruthy();
+    });
+
+    it('내용은 필수 입력 값 입니다.', () => {
+      const input = {
+        rating: 4,
+      };
+
+      const parse = schema.safeParse(input);
+      expect(parse.success).toBeFalsy();
+      expect(parse.error?.issues[0].message).toBe('내용은 필수 입력입니다');
+    });
+
+    it('내용은 최소 1자 이상이어야 합니다.', () => {
+      const input = {
+        rating: 4,
+        content: '',
+      };
+
+      const parse = schema.safeParse(input);
+      expect(parse.success).toBeFalsy();
+      expect(parse.error?.issues[0].message).toBe('내용은 최소 1자 이상이어야 합니다');
+    });
+
+    it('평점은 숫자가 아니라면 유효성 검사에 실패해야 합니다', () => {
+      const input = {
+        rating: '5',
+        content: '좋은 숙소였습니다',
+      };
+
+      const parse = schema.safeParse(input);
+      expect(parse.success).toBeFalsy();
+      expect(parse.error?.issues[0].message).toBe('평점은 숫자여야 합니다');
+    });
+
+    it('평점은 1점 미만일 수 없습니다.', () => {
+      const input = {
+        rating: 0,
+        content: '좋은 숙소였습니다',
+      };
+
+      const parse = schema.safeParse(input);
+      expect(parse.success).toBeFalsy();
+      expect(parse.error?.issues[0].message).toBe('평점은 1에서 5 사이여야 합니다');
+    });
+
+    it('평점은 5점을 넘을 수 없습니다.', () => {
+      const input = {
+        rating: 6,
+        content: '좋은 숙소였습니다',
+      };
+
+      const parse = schema.safeParse(input);
+      expect(parse.success).toBeFalsy();
+      expect(parse.error?.issues[0].message).toBe('평점은 1에서 5 사이여야 합니다');
+    });
   });
+};
 
-  it('평점은 숫자가 아니라면 유효성 검사에 실패해야 합니다', () => {
-    const input = {
-      rating: '5',
-      content: '좋은 숙소였습니다',
-    };
-
-    const parse = createReviewSchema.safeParse(input);
-    expect(parse.success).toBeFalsy();
-    expect(parse.error?.issues[0].message).toBe('평점은 숫자여야 합니다');
-  });
-
-  it('평점은 1점 미만일 수 없습니다.', () => {
-    const input = {
-      rating: 0,
-      content: '좋은 숙소였습니다',
-    };
-
-    const parse = createReviewSchema.safeParse(input);
-    expect(parse.success).toBeFalsy();
-    expect(parse.error?.issues[0].message).toBe('평점은 1에서 5 사이여야 합니다');
-  });
-
-  it('평점은 5점을 넘을 수 없습니다.', () => {
-    const input = {
-      rating: 6,
-      content: '좋은 숙소였습니다',
-    };
-
-    const parse = createReviewSchema.safeParse(input);
-    expect(parse.success).toBeFalsy();
-    expect(parse.error?.issues[0].message).toBe('평점은 1에서 5 사이여야 합니다');
-  });
-});
-
-describe('updateReviewSchema', () => {
-  it('입력 값이 정상이라면 유효성 검사를 통과해야 합니다', () => {
-    const input = {
-      rating: 5,
-      content: '좋은 숙소였습니다',
-    };
-
-    expect(createReviewSchema.safeParse(input).success).toBeTruthy();
-  });
-
-  it('평점은 숫자가 아니라면 유효성 검사에 실패해야 합니다', () => {
-    const input = {
-      rating: '5',
-      content: '좋은 숙소였습니다',
-    };
-
-    const parse = createReviewSchema.safeParse(input);
-    expect(parse.success).toBeFalsy();
-    expect(parse.error?.issues[0].message).toBe('평점은 숫자여야 합니다');
-  });
-
-  it('평점은 1점 미만일 수 없습니다.', () => {
-    const input = {
-      rating: 0,
-      content: '좋은 숙소였습니다',
-    };
-
-    const parse = createReviewSchema.safeParse(input);
-    expect(parse.success).toBeFalsy();
-    expect(parse.error?.issues[0].message).toBe('평점은 1에서 5 사이여야 합니다');
-  });
-
-  it('평점은 5점을 넘을 수 없습니다.', () => {
-    const input = {
-      rating: 6,
-      content: '좋은 숙소였습니다',
-    };
-
-    const parse = createReviewSchema.safeParse(input);
-    expect(parse.success).toBeFalsy();
-    expect(parse.error?.issues[0].message).toBe('평점은 1에서 5 사이여야 합니다');
-  });
-});
+testReviewSchema('createReviewSchema', createReviewSchema);
+testReviewSchema('updateReviewSchema', updateReviewSchema);
--- src/schemas/review.ts
+++ src/schemas/review.ts
@@ -1,16 +1,25 @@
 import { schemaType } from '@/schemas';
 import { z } from 'zod';
 
+const contentType = z
+  .string({
+    required_error: '내용은 필수 입력입니다',
+  })
+  .min(1, {
+    message: '내용은 최소 1자 이상이어야 합니다',
+  })
+  .max(1000);
+
 export const updateReviewSchema = z.object({
   rating: schemaType.rating,
-  content: z.string().min(1).max(1000),
+  content: contentType,
 });
 
 export type UpdateReviewInput = z.infer<typeof updateReviewSchema>;
 
 export const createReviewSchema = z.object({
   rating: schemaType.rating,
-  content: z.string().min(1).max(1000),
+  content: contentType,
 });
 
 export type CreateReviewInput = z.infer<typeof createReviewSchema>;
