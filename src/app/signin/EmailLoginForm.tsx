720deac801b204101567b57a31ef89bf24c40349
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-12-17 21:10:01 +0900

    refactor: 이메일 로그인 폼 관리 커스텀 훅으로 분리

--- src/app/signin/EmailLoginForm.tsx
+++ src/app/signin/EmailLoginForm.tsx
@@ -1,33 +1,22 @@
 'use client';
 
-import { useFormState } from 'react-dom';
 import Link from 'next/link';
 import { EmailLoginFormBtn } from '@/app/signin/EmailLoginFormBtn';
-import { handleEmailLogin } from '@/app/signin/action';
-import { FormState } from '@/types/login';
+import { useEmailLoginForm } from '@/hooks/useEmailLoginForm';
 import { getWebmailUrl } from '@/utils/email';
 
-const initialState: FormState = {
-  success: false,
-  errors: {
-    email: [],
-    server: [],
-  },
-  email: '',
-};
-
 export const EmailLoginForm = () => {
-  const [state, dispatch] = useFormState(handleEmailLogin, initialState);
+  const { dispatch, email, success, errors } = useEmailLoginForm();
 
   return (
     <div className="space-y-4">
       {/* 이메일 전송 완료 후 안내 문구 */}
-      {state.success && state.email && (
+      {success && email && (
         <div className="rounded-md bg-green-50 px-2 py-4">
           <div className="flex items-end gap-4">
             <p className="text-sm text-green-700">이메일로 전송된 로그인 링크를 확인하세요</p>
             <Link
-              href={getWebmailUrl(state.email)}
+              href={getWebmailUrl(email)}
               target="_blank"
               rel="noopener noreferrer"
               className="text-sm text-green-700 underline hover:text-green-800"
@@ -53,15 +42,13 @@ export const EmailLoginForm = () => {
           />
 
           {/* 이메일 포맷 관련 에러 문구 */}
-          {state.errors.email.length > 0 && (
-            <p className="text-sm text-red-500">{state.errors.email[0]}</p>
-          )}
+          {errors.email.length > 0 && <p className="text-sm text-red-500">{errors.email[0]}</p>}
         </div>
 
         {/* 서버 관련 에러 문구 */}
-        {state.errors.server.length > 0 && (
+        {errors.server.length > 0 && (
           <div className="rounded-md bg-red-50 px-2 py-4">
-            <p className="text-sm text-red-500">{state.errors.server[0]}</p>
+            <p className="text-sm text-red-500">{errors.server[0]}</p>
           </div>
         )}
 
---
+++ src/hooks/useEmailLoginForm.ts
@@ -0,0 +1,23 @@
+import { useFormState } from 'react-dom';
+import { FormState } from '@/types/login';
+import { handleEmailLogin } from '@/app/signin/action';
+
+const initialState: FormState = {
+  success: false,
+  errors: {
+    email: [],
+    server: [],
+  },
+  email: '',
+};
+
+export const useEmailLoginForm = () => {
+  const [state, dispatch] = useFormState(handleEmailLogin, initialState);
+  
+  return {
+    dispatch,
+    email: state.email,
+    success: state.success,
+    errors: state.errors,
+  };
+};
