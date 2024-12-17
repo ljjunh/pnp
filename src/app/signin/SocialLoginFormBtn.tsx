a99d55dd6310bae15d93ab4b6621276cad10567f
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-12-17 20:49:23 +0900

    style: 소셜로그인 버튼 disabled 색상 변경

--- src/app/signin/SocialLoginFormBtn.tsx
+++ src/app/signin/SocialLoginFormBtn.tsx
@@ -23,7 +23,7 @@ export function SocialLoginFormBtn({ text }: { text: SocialLoginType }) {
   return (
     <button
       disabled={pending}
-      className="flex w-full items-center justify-between rounded-md border border-gray-500 px-4 py-2 hover:bg-gray-100 disabled:cursor-not-allowed disabled:bg-neutral-400 disabled:text-neutral-300"
+      className="flex w-full items-center justify-between rounded-md border border-gray-500 px-4 py-2 hover:bg-gray-100 disabled:cursor-not-allowed disabled:bg-neutral-200 disabled:text-neutral-100"
     >
       {getIcon()}
       <div>{pending ? '로그인 중' : text}</div>
