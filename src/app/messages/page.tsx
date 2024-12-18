17954ef1c716a250cc071f91271b8a643090d008
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-12-18 15:25:27 +0900

    feat: 메세지 앱라우트 변경

---
+++ src/app/messages/page.tsx
@@ -0,0 +1,15 @@
+export default function Messages() {
+  return (
+    <div>
+      <div>
+        레프트
+      </div>
+      <div>
+        센터
+      </div>
+      <div>
+        라이트
+      </div>
+    </div>
+  );
+}
