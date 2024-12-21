f53b081a8064f64aa33e4dbed915529ad04e80ae
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-12-21 19:44:00 +0900

    feat: 예약 정보 UI 구현

--- src/app/messages/components/ReservationInfo.tsx
+++ src/app/messages/components/ReservationInfo.tsx
@@ -1,7 +1,24 @@
+import RoomCardCarousel from '@/components/common/Card/RoomCardCarousel';
+import { BsHouseExclamation } from 'react-icons/bs';
+import { FaAirbnb } from 'react-icons/fa';
+import { IoIosArrowForward } from 'react-icons/io';
+import { IoIosHelpCircle } from 'react-icons/io';
+import { TbWorld } from 'react-icons/tb';
+import { TbReceiptFilled } from 'react-icons/tb';
+
 interface ReservationInfoProps {
   showReservation: boolean;
   onToggleReservation: () => void;
 }
+
+const images = [
+  '/images/05.avif',
+  '/images/02.avif',
+  '/images/04.avif',
+  '/images/01.avif',
+  '/images/03.avif',
+  '/images/06.avif',
+];
 
 export function ReservationInfo({ showReservation, onToggleReservation }: ReservationInfoProps) {
   return (
@@ -10,11 +27,186 @@ export function ReservationInfo({ showReservation, onToggleReservation }: Reserv
         showReservation ? 'w-1/4 opacity-100' : 'w-0 opacity-0'
       }`}
     >
-      <header className="flex h-20 items-center justify-between border-b px-8 py-6">
+      <div className="flex h-20 items-center justify-between border-b px-8 py-6">
         <h1 className="text-2xl">예약</h1>
         <button onClick={onToggleReservation}>X</button>
-      </header>
-      <div className="flex-1 overflow-y-scroll px-8 py-6">숙소 정보</div>
+      </div>
+
+      <div className="flex flex-1 flex-col gap-3 overflow-y-scroll px-2">
+        <div className="flex flex-col gap-5 px-4 pt-4">
+          <h1 className="text-2xl font-medium">호스트 이름의 숙소</h1>
+          <RoomCardCarousel images={images} />
+          <div className="flex flex-col gap-1">
+            <h3 className="font-bold">예약이 종료 되었습니다.</h3>
+            <p>
+              여행 중 발생한 문제로 인해 상대방에게 금액을 지급하거나 요청해야 할 경우, 해결 센터를
+              이용하시기 바랍니다.
+            </p>
+          </div>
+          <button className="rounded-xl border border-black py-3 hover:bg-gray-100">
+            지급 또는 결제 요청
+          </button>
+          <hr />
+          <div className="flex justify-between">
+            <div className="flex-1 border-r">
+              <p>체크인</p>
+              <p>1월 11일 (화)</p>
+              <p>오후 4:00</p>
+            </div>
+            <div className="flex-1 text-right">
+              <p>체크인</p>
+              <p>1월 11일 (화)</p>
+              <p>오후 4:00</p>
+            </div>
+          </div>
+          <hr />
+          {/* onClick 숙소 상세 페이지로 */}
+        </div>
+        <button>
+          <div className="flex gap-3 px-4 py-4 hover:bg-gray-100">
+            <BsHouseExclamation className="size-8" />
+            <div className="flex flex-col items-start">
+              <h1>숙소</h1>
+              <p className="line-clamp-1">고색역 도보 5분 넷플릭스 웨이브 쿠팡 플레이</p>
+            </div>
+          </div>
+        </button>
+        <div className="border-b-8" />
+        <div className="flex flex-col gap-5 px-4">
+          <h1 className="text-xl font-medium">예약 세부정보</h1>
+          <div>
+            <h2 className="font-semibold">게스트</h2>
+            <p>게스트 1명</p>
+          </div>
+          <hr />
+          <div>
+            <h2 className="font-semibold">예약 번호</h2>
+            <p>HM9DZBXA8R</p>
+          </div>
+          <hr />
+          <div>
+            <h2 className="font-semibold">환불 정책</h2>
+          </div>
+          <div>
+            <div className="-mx-4 px-4 hover:bg-gray-100">
+              <hr />
+              <button className="w-full py-3">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-3">
+                    <TbWorld />
+                    <p>여행 일정표 PDF로 받기</p>
+                  </div>
+                  <IoIosArrowForward />
+                </div>
+              </button>
+
+              <hr />
+            </div>
+            <div className="-mx-4 px-4 hover:bg-gray-100">
+              <button className="w-full py-3">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-3">
+                    <TbReceiptFilled />
+                    <p>영수증 받기</p>
+                  </div>
+                  <IoIosArrowForward />
+                </div>
+              </button>
+              <hr />
+            </div>
+          </div>
+        </div>
+        <div className="border-b-8" />
+        <div className="flex flex-col gap-5 px-4">
+          <h2 className="text-xl font-semibold">숙소 이용 규칙 및 안내</h2>
+          <div>
+            <h3 className="font-bold">숙소 이용규칙</h3>
+            <p>셀프 체크인:스마트 도어록</p>
+            <p>게스트 정원 1명</p>
+            <p>반려동물 동반 불가</p>
+            <p className="cursor-pointer underline">더 보기</p>
+          </div>
+          <div className="-mx-4 px-4 hover:bg-gray-100">
+            <hr />
+            <button className="w-full py-3">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                  <BsHouseExclamation />
+                  <p>숙소 보기</p>
+                </div>
+                <IoIosArrowForward />
+              </div>
+            </button>
+          </div>
+        </div>
+
+        <div className="border-b-8" />
+        <div className="flex flex-col gap-5 px-4">
+          <div className="flex justify-between">
+            <h2 className="text-xl font-semibold">호스트: 호스트 이름</h2>
+            <div>호스트 프로필</div>
+          </div>
+          <p className="cursor-pointer underline">더 보기</p>
+        </div>
+
+        <div className="border-b-8" />
+        <div className="flex flex-col gap-5 px-4">
+          <h2 className="text-xl font-semibold">결제 정보</h2>
+          <div>
+            <h3 className="font-bold">결제한 금액</h3>
+            <p>₩207,953.00</p>
+          </div>
+          <div className="-mx-4 px-4 hover:bg-gray-100">
+            <hr />
+            <button className="w-full py-3">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                  <TbReceiptFilled />
+
+                  <p>영수증 받기</p>
+                </div>
+                <IoIosArrowForward />
+              </div>
+            </button>
+          </div>
+        </div>
+
+        <div className="border-b-8" />
+        <div className="flex flex-col gap-5 px-4">
+          <div>
+            <h2>언제든 도움을 받으세요</h2>
+            <p>도움이 필요하세요? 전 세계 어디서든 연중무휴로 지원해드립니다.</p>
+          </div>
+          <div>
+            <div className="-mx-4 px-4 hover:bg-gray-100">
+              <hr />
+              <button className="w-full py-3">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-3">
+                    <FaAirbnb />
+                    <p>에어비앤비 고객지원 팀에 연락하기</p>
+                  </div>
+                  <IoIosArrowForward />
+                </div>
+              </button>
+
+              <hr />
+            </div>
+            <div className="-mx-4 px-4 hover:bg-gray-100">
+              <button className="w-full py-3">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-3">
+                    <IoIosHelpCircle />
+                    <p>도움말 센터 방문하기</p>
+                  </div>
+                  <IoIosArrowForward />
+                </div>
+              </button>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
