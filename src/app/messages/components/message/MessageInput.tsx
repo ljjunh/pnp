a57e6cba3380f4e32401385f1506d6c91a0e55c8
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-12-23 09:07:16 +0900

    feat: 이미지 파일 첨부 input 구현

--- src/app/messages/components/message/MessageInput.tsx
+++ src/app/messages/components/message/MessageInput.tsx
@@ -3,8 +3,19 @@ import { IoImageOutline } from 'react-icons/io5';
 export function MessageInput() {
   return (
     <div className="flex h-24 items-center gap-3 px-10">
-      <IoImageOutline className="size-7" />
-      <form className="relative w-full text-base">
+      <form className="relative flex w-full items-center gap-3 text-base">
+        <label
+          htmlFor="image-upload"
+          className="cursor-pointer"
+        >
+          <IoImageOutline className="size-7" />
+          <input
+            id="image-upload"
+            type="file"
+            accept="image/*"
+            className="hidden"
+          />
+        </label>
         <input
           type="text"
           placeholder="메세지를 입력하세요."
@@ -12,5 +23,6 @@ export function MessageInput() {
         />
       </form>
     </div>
+
   );
 }
