013a69de41b93a5aab72bbb30d185170d5642e57
sunsuking <sunsuking@gmail.com>
2024-12-23 20:48:39 +0900

    feat: 일단 보류

--- src/app/api/socket/route.ts
+++ src/app/api/socket/route.ts
@@ -1,42 +1,51 @@
+import { NextApiResponse } from 'next';
 import { NextRequest } from 'next/server';
 import { messageInputSchema } from '@/schemas';
+import { Server as NetServer } from 'http';
 import { Server } from 'socket.io';
-import { NextIOResponse } from '@/types/next';
+import { Socket } from 'socket.io-client';
+
+export type NextIOResponse = NextApiResponse & {
+  socket: Socket & {
+    server?: NetServer & {
+      io: Server;
+    };
+  };
+};
 
 export async function GET(request: NextRequest, response: NextIOResponse) {
-  if (response.socket.server.io) {
+  if (!response.socket?.server.io) {
+    const httpServer = response.socket?.server as NetServer;
+    const io = new Server(httpServer, {
+      cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+      },
+    });
+
+    io.on('connection', (socket) => {
+      socket.on('enter', async (roomId) => {
+        socket.join(roomId);
+        console.log(`user entered room: ${roomId}`);
+      });
+
+      socket.on('message', async (message) => {
+        try {
+          const data = messageInputSchema.parse(message);
+        } catch (error) {
+          console.error(error);
+          socket.emit('error', error);
+        }
+      });
+
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+    });
+
+    response.socket.server.io = io;
     response.end();
-    return;
   }
 
-  const io = new Server(response.socket.server, {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST'],
-    },
-  });
-
-  response.socket.server.io = io;
-
-  io.on('connection', (socket) => {
-    socket.on('enter', async (roomId) => {
-      socket.join(roomId);
-      console.log(`user entered room: ${roomId}`);
-    });
-
-    socket.on('message', async (message) => {
-      try {
-        const data = messageInputSchema.parse(message);
-      } catch (error) {
-        console.error(error);
-        socket.emit('error', error);
-      }
-    });
-
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-
-  response.end();
+  return new Response(null, { status: 201 });
 }
--- src/app/messages/components/message/MessageContent.tsx
+++ src/app/messages/components/message/MessageContent.tsx
@@ -12,6 +12,7 @@ interface DateGroup {
 
 const formatDate = (dateStr: string) => {
   const date = new Date(dateStr);
+
   return new Intl.DateTimeFormat('ko-KR', {
     year: 'numeric',
     month: 'long',
@@ -23,6 +24,7 @@ const formatDate = (dateStr: string) => {
 const formatTime = (timeStr: string) => {
   const [hours, minutes] = timeStr.split(':');
   const hour = parseInt(hours);
+
   return `${hour < 12 ? '오전' : '오후'} ${hour < 12 ? hour : hour - 12}:${minutes}`;
 };
 
