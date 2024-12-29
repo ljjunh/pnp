8d70068705394f2b209626983238f586e16e115b
junhee <limjunhee4575@gmail.com>
2024-12-29 23:49:07 +0900

    feat : 리뷰 삭제 버튼 퍼블리싱

---
+++ src/app/(header-footer)/rooms/[id]/components/review/RoomReviewDeleteButton.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function RoomReviewDeleteButton() {
+  return <button className="hover:text-black">삭제</button>;
+}
--- src/app/(header-footer)/rooms/[id]/components/review/RoomReviewItem.tsx
+++ src/app/(header-footer)/rooms/[id]/components/review/RoomReviewItem.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import Image from 'next/image';
+import RoomReviewDeleteButton from '@/app/(header-footer)/rooms/[id]/components/review/RoomReviewDeleteButton';
+import { auth } from '@/auth';
 import { Review } from '@/types/review';
+import { calculateAverageRating } from '@/utils/calculateAverageRating';
 import { formatElapsedTime } from '@/utils/formatElapsedTime';
 import { formatRelativeDate } from '@/utils/formatRelativeDate';
 import { IoIosStar } from 'react-icons/io';
@@ -16,7 +19,7 @@ interface RoomReviewItemProps {
   user: Review['user'];
 }
 
-export default function RoomReviewItem({
+export default async function RoomReviewItem({
   accuracy,
   communication,
   cleanliness,
@@ -27,41 +30,59 @@ export default function RoomReviewItem({
   createdAt,
   user,
 }: RoomReviewItemProps) {
-  const rating =
-    [accuracy, communication, cleanliness, location, checkIn, value].reduce(
-      (sum, score) => sum + score,
-      0,
-    ) / 6;
+  const session = await auth();
 
-  const StarRating = ({ rating }: { rating: number }) => {
-    return (
-      <div className="flex">
-        {[1, 2, 3, 4, 5].map((star) => (
-          <span
-            key={star}
-            className="text-xs"
-          >
-            <IoIosStar className={rating >= star ? 'text-black' : 'text-gray-200'} />
-          </span>
-        ))}
-      </div>
-    );
-  };
+  const rating = calculateAverageRating([
+    accuracy,
+    communication,
+    cleanliness,
+    location,
+    checkIn,
+    value,
+  ]);
 
   return (
     <div>
       <div className="flex flex-col gap-3">
-        <div className="flex items-center gap-3">
-          <div className="h-12 w-12 rounded-full bg-black"></div>
-          <div>
-            <div className="font-semibold">{user.name}</div>
-            <div className="text-sm text-shade-02">
-              에어비앤비 가입 기간 {formatElapsedTime(new Date(user.host.hostStartedAt))}
+        <div className="flex items-center justify-between">
+          <div className="flex gap-3">
+            {user.image ? (
+              <div className="relative h-12 w-12">
+                <Image
+                  src={user.image}
+                  alt="user-image"
+                  fill
+                  className="rounded-full object-cover"
+                />
+              </div>
+            ) : (
+              <div className="h-12 w-12 rounded-full bg-black"></div>
+            )}
+            <div>
+              <div className="font-semibold">{user.name}</div>
+              <div className="text-sm text-shade-02">
+                에어비앤비 가입 기간 {formatElapsedTime(new Date(user.host.hostStartedAt))}
+              </div>
             </div>
           </div>
+          {session?.user.id === user.id && (
+            <div className="flex gap-2 text-sm text-neutral-07">
+              <button className="hover:text-black">수정</button>
+              <RoomReviewDeleteButton />
+            </div>
+          )}
         </div>
         <div className="flex items-center gap-1 text-sm">
-          <StarRating rating={rating} />
+          <div className="flex">
+            {[1, 2, 3, 4, 5].map((star) => (
+              <span
+                key={star}
+                className="text-xs"
+              >
+                <IoIosStar className={rating >= star ? 'text-black' : 'text-gray-200'} />
+              </span>
+            ))}
+          </div>
           <span className="text-neutral-07">·</span>
           <div>{formatRelativeDate(new Date(createdAt))}</div>
         </div>
