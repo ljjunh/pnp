250af17e25ba02d954f5f049ead392586916c71e
junhee <limjunhee4575@gmail.com>
2025-01-02 17:33:58 +0900

    test : 스크랩 추가 테스트코드 작성

--- src/mocks/handlers/rooms/index.ts
+++ src/mocks/handlers/rooms/index.ts
@@ -1,4 +1,5 @@
+import { createScrapHandler } from '@/mocks/handlers/rooms/createScrap';
 import { getRoomHandler } from '@/mocks/handlers/rooms/getRoom';
 import { getScrapHandler } from '@/mocks/handlers/rooms/getScrap';
 
-export const roomHandlers = [getRoomHandler, getScrapHandler];
+export const roomHandlers = [getRoomHandler, getScrapHandler, createScrapHandler];
