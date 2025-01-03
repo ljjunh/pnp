2772ee62e9d1e3a11c28fae66004233fd15de24f
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2025-01-03 19:34:02 +0900

    docs: JSDoc 추가

--- src/apis/trips/queries.ts
+++ src/apis/trips/queries.ts
@@ -3,6 +3,12 @@ import { CustomError } from '@/errors';
 import { ReservationTrip } from '@/types/reservation';
 import { httpClient } from '../core/httpClient';
 
+/**
+ * 예약된 여행 정보를 조회합니다.
+ * @returns {Promise<ReservationTrip[]>} 에약된 여행 정보
+ * @remarks 숙소를 찾을 수 없는 경우 Next.js not-found 페이지를 렌더링
+ */
+
 export async function getReservationTrip(): Promise<ReservationTrip[]> {
   try {
     const response = await httpClient.get<ReservationTrip[]>('/reservation');
--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,3 +1,14 @@
+/**
+* 날짜 문자열을 'YYYY년 MM월 DD일' 형식으로 변환합니다.
+* 
+* @param {Date} dateString - 변환할 날짜
+* @returns {string} 'YYYY년 MM월 DD일' 형식의 날짜 문자열
+* 
+* @example
+* // '2024년 01월 03일' 반환
+* formatDate(new Date('2024-01-03'))
+*/
+
 export function formatDate(dateString: Date) {
   const date = new Date(dateString);
 
