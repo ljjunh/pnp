3cffe0fb3f468bc5d7d47fe39ff9f279fc3ca1b3
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2025-01-07 00:31:41 +0900

    chore: 주석 추가

--- src/app/(user)/trips/page.tsx
+++ src/app/(user)/trips/page.tsx
@@ -7,18 +7,17 @@ import { UpcommingReservationCard } from './components/UpcommingReservationCard'
 import { categorizeReservations } from '@/utils/categorizeReservation';
 
 export const dynamic = 'force-dynamic'; // 동적 렌더링
-// export const revalidate = 60; // 60초 ISR
 
 export default async function Trips() {
   const reservations = await getReservationTrip();
   const { upcomingReservations, previousReservations } = categorizeReservations(reservations);
-  // const upcomingReservations = []
 
   return (
     <main className="flex w-full justify-center">
       <div className="w-full max-w-screen-2xl px-20 py-12">
         <h1 className="text-3xl font-semibold">여행</h1>
 
+        {/* 다가오는 여행 예약 리스트 */}
         {upcomingReservations.length > 0 ? (
           <section>
             <h3 className="mb-2 mt-12 text-xl font-medium">다가오는 여행</h3>
@@ -36,13 +35,15 @@ export default async function Trips() {
             </div>
           </section>
         ) : (
+
+          // 에약된 여행 일정 없는 경우
           <NoReservation />
         )}
 
+        {/* 지난 여행 리스트 */}
         {previousReservations.length > 0 && (
           <section>
             <h2 className="my-6 text-xl">이전 여행지</h2>
-
             <div className="grid grid-cols-3 gap-5">
               {previousReservations.map((reservation) => (
                 <PreviousReservationCard
@@ -57,8 +58,9 @@ export default async function Trips() {
             </div>
           </section>
         )}
-
         <hr className="mb-5 mt-12" />
+
+        {/* 지원 센터 */}
         <Help />
       </div>
     </main>
