55f28da91b5720e054b50f72fcb3f22334ed7b77
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2025-02-04 22:58:51 +0900

    feat: 최근 조회 숙소 상세페이지 링크 연결

--- src/app/(user)/recently-viewed/components/RoomCard.tsx
+++ src/app/(user)/recently-viewed/components/RoomCard.tsx
@@ -1,5 +1,7 @@
+import { ROUTES } from '@/constants/routeURL';
 import { RecentViewResponse } from '@/types/recent';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface RoomCardProps {
   isEdit: boolean;
@@ -8,7 +10,7 @@ interface RoomCardProps {
 
 export function RoomCard({ isEdit, recentView }: RoomCardProps) {
   return (
-    <div>
+    <Link href={ROUTES.ROOMS.DETAIL(recentView.id)}>
       <div className="relative aspect-square">
         {isEdit === true && (
           <div className="absolute left-3 top-3 z-10 cursor-pointer rounded-full bg-slate-50 px-2 shadow-lg hover:scale-105 hover:bg-white">
@@ -26,6 +28,6 @@ export function RoomCard({ isEdit, recentView }: RoomCardProps) {
         <h3 className="text-base">{recentView.location}</h3>
         <p className="text-sm text-neutral-500 overflow-hidden whitespace-nowrap text-overflow-ellipsis">{recentView.title}</p>
       </div>
-    </div>
+    </Link>
   );
 }
