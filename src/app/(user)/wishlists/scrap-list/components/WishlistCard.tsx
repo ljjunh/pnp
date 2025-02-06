0e490004d182ee28d75b13c887b53a5b596eb46a
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2025-02-06 12:35:41 +0900

    feat: 스크랩 api 연동

--- src/app/(user)/wishlists/scrap-list/components/WishlistCard.tsx
+++ src/app/(user)/wishlists/scrap-list/components/WishlistCard.tsx
@@ -1,19 +1,36 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { ScrapResponse } from '@/types/scrap';
+import { ROUTES } from '@/constants/routeURL';
 
-export function WishlistCard() {
+interface WishlistCardProps {
+  scrapId: ScrapResponse['id'];
+  scrapThumbnail: ScrapResponse['thumbnail'];
+  scrapLocation: ScrapResponse['location'];
+  scrapTitle: ScrapResponse['title'];
+}
+
+export function WishlistCard({
+  scrapId,
+  scrapThumbnail,
+  scrapLocation,
+  scrapTitle,
+}: WishlistCardProps) {
   return (
     <div>
       <div className="relative aspect-square">
-        <Image
-          src="/images/05.avif"
-          alt="숙소 사진"
-          fill
-          className="rounded-2xl object-cover"
-        />
+        <Link href={ROUTES.ROOMS.DETAIL(scrapId)}>
+          <Image
+            src={scrapThumbnail!}
+            alt="숙소 사진"
+            fill
+            className="rounded-2xl object-cover"
+          />
+        </Link>
       </div>
       <div className="mt-2">
-        <h3 className="text-base">호스트 이름의 집</h3>
-        <p className="text-sm text-neutral-500">침대 1개 ㆍ 침실 1개</p>
+        <h3 className="text-base">{scrapLocation}</h3>
+        <p className="text-sm text-neutral-500">{scrapTitle}</p>
       </div>
       <div className="mt-2 w-full rounded-xl bg-gray-100 p-3">
         <p className="cursor-pointer text-sm underline">메모 추가</p>
--- src/app/(user)/wishlists/scrap-list/page.tsx
+++ src/app/(user)/wishlists/scrap-list/page.tsx
@@ -2,11 +2,14 @@ import { BackBtn } from '@/app/(user)/components/BackBtn';
 import { Btn } from '@/app/(user)/wishlists/scrap-list/components/Btn';
 import { SettingBtn } from '@/app/(user)/wishlists/scrap-list/components/SettingBtn';
 import { WishlistCard } from '@/app/(user)/wishlists/scrap-list/components/WishlistCard';
+import { getScrapList } from '@/apis/wishlists/queries';
 import { ROUTES } from '@/constants/routeURL';
 
 const filters = ['날짜 입력하기', '게스트 1명', '공유하기'];
 
-export default function WishlistsDetail() {
+export default async function WishlistsDetail() {
+  const scrapList = await getScrapList();
+
   return (
     <div className="flex w-full">
       <div className="w-[65%] px-5 pt-5">
@@ -14,7 +17,7 @@ export default function WishlistsDetail() {
           <BackBtn url={ROUTES.USER.WISHLISTS} />
           <SettingBtn />
         </div>
-        <h1 className="my-5 text-2xl font-medium">위시리스트 폴더 명</h1>
+        <h1 className="my-5 text-2xl font-medium">위시리스트</h1>
         <div className="flex gap-2">
           {filters.map((filter, index) => (
             <Btn
@@ -24,7 +27,15 @@ export default function WishlistsDetail() {
           ))}
         </div>
         <div className="mt-10 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
-          <WishlistCard />
+          {scrapList.map((scrapItem) => (
+            <WishlistCard
+              key={scrapItem.id}
+              scrapId={scrapItem.id}
+              scrapThumbnail={scrapItem.thumbnail}
+              scrapLocation={scrapItem.location}
+              scrapTitle={scrapItem.title}
+            />
+          ))}
         </div>
       </div>
       <div className="h-screen w-[35%] bg-black text-white">지도</div>
